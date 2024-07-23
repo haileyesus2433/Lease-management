1dd0e79f7cde4f3a5d14fcb15c82c25ce73d02b2
Haileyesus <Haileyesusbe@gmail.com>
2024-07-23 17:56:55 -0400

    Fix small bug

--- app/page.tsx
+++ app/page.tsx
@@ -7,5 +7,7 @@ export default async function Home() {
   const session = await getServerSession(authOptions);
   if (!session) {
     redirect("/sign-in");
+  } else {
+    redirect("/home");
   }
 }
